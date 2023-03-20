8197d12dda5b98fba48ac8cafa91958d1b3511e6
SeregaLi <kaprall13rus@gmail.com>
2023-03-20 19:13:13 +0300

    final 2

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
-import ccxt
-import datetime
+import requests
 import time
+import threading
 
-exchange = ccxt.binance()  # Создаем объект биржи binance
 
-ticker = exchange.fetch_ticker('ETH/USDT')  # Получаем текущую цену ETHUSDT
-current_price = ticker['last']
+def get_eth_price() -> float:
+    """Функция для получения текущей цены фьючерса ETHUSDT"""
+    url = 'https://fapi.binance.com/fapi/v1/ticker/price'
+    params = {'symbol': 'ETHUSDT'}
+    response = requests.get(url, params=params)
+    data = response.json()
+    return float(data['price'])
 
-while True:  # Основной цикл программы
 
-    time.sleep(1)  # Ждем 1 секунду, чтобы не перегружать биржу запросами
+def get_btc_price() -> float:
+    """Функция для получения текущей цены BTCUSDT"""
+    url = 'https://api.binance.com/api/v3/ticker/price'
+    params = {'symbol': 'BTCUSDT'}
+    response = requests.get(url, params=params)
+    data = response.json()
+    return float(data['price'])
 
-    ticker = exchange.fetch_ticker('ETH/USDT')  # Получаем текущую цену ETHUSDT
-    current_price = ticker['last']
 
-    past_time = datetime.datetime.now() - datetime.timedelta(
-        minutes=60)  # Получаем время 60 минут назад
+def get_eth_btc_influence() -> float:
+    """Функция для получения коэффициента влияния цены BTCUSDT на цену ETHUSDT"""
+    url = 'https://fapi.binance.com/fapi/v1/premiumIndex'
+    params = {'symbol': 'ETHUSDT'}
+    response = requests.get(url, params=params)
+    data = response.json()
+    return float(data['lastFundingRate']) * 3
 
-    past_ticker = exchange.fetch_ohlcv('ETH/USDT', '1m',
-                                       since=int(past_time.timestamp() * 1000),
-                                       limit=1)  # Получаем цену ETHUSDT 60 минут назад
-    past_price = past_ticker[0][4]
 
-    price_change = (
-                    current_price / past_price - 1) * 100  # Определяем процентное изменение цены
+def calculate_eth_price(exclude_btc_influence: float) -> float:
+    """Функция для расчета цены ETHUSDT, исключив его движения, вызванные влиянием цены BTCUSDT"""
+    eth_price = get_eth_price()
+    btc_price = get_btc_price()
+    eth_btc_influence = get_eth_btc_influence()
+    eth_price -= (btc_price * eth_btc_influence * exclude_btc_influence)
+    return eth_price
 
-    if abs(price_change) > 1:  # Если процентное изменение больше 1%, выводим сообщение в консоль
-        if price_change > 0:
-            print("Цена ETHUSDT выросла на", round(price_change, 2),
-                  "% за последний час")
-        else:
-            print("Цена ETHUSDT упала на", round(price_change, 2),
-                  "% за последний час")
+
+def monitor_price_change() -> None:
+    """Функция для отслеживания изменения цены ETHUSDT и вывода сообщения в консоль"""
+    exclude_btc_influence = 0
+    while True:
+        eth_price = calculate_eth_price(exclude_btc_influence)
+        time.sleep(1)
+        eth_price_last_hour = calculate_eth_price(exclude_btc_influence)
+        eth_price_change = (eth_price_last_hour / eth_price) - 1
+        if abs(eth_price_change) >= 0.01:
+            print(f'Price change detected: {eth_price_change * 100:.2f}%')
+        exclude_btc_influence = (eth_price_last_hour / eth_price) - 1
+
+
+if __name__ == '__main__':
+    t = threading.Thread(target=monitor_price_change)
+    t.start()
+    t.join()
